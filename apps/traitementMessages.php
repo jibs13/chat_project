0d1dfad5e11b615b711e583085f7e674d4b6cc46
titineo <laetitia.werle@yahoo.fr>
2017-02-17 14:04:58 +0100

    traitementMessages.php V2

---
+++ apps/traitementMessages.php
@@ -0,0 +1,31 @@
+<?php
+var_dump($_POST);
+
+if(isset($_POST["content"], $_SESSION['id']))
+{
+	$content=$_POST["content"];
+	if (strlen($content) > 4095)
+	{
+		$errors[] = "Contenu trop long (> 4095)";
+	}
+	else if (strlen($content) < 65)
+	{
+		$errors[] = "Contenu trop court (< 65)";
+	}
+	if (count($errors) == 0)
+	{
+		$content = mysqli_real_escape_string($db, $content); //pour la secruité
+		$res=mysqli_query($db, "INSERT INTO messages (content) VALUES('".$content."', '".$_SESSION['id']."')");
+		if ($res)
+		{
+			header("Location: index.php?page=messages");
+			exit;//toujours exit si header
+		}
+		else
+		{
+			$errors[] = "Erreur interne";
+			$errors[] = mysqli_error($db);
+		}
+	}
+}
+?>
