401e2da3dc0b9d7da9c3bbae58e6e1d43a343806
mimimolette <maudfroehlicher@ymail.com>
2017-02-17 13:41:55 +0100

    messages.php V.1.1

---
+++ apps/messages.php
@@ -0,0 +1,16 @@
+<?php 
+
+
+// while ($article = mysqli_fetch_assoc($res))
+// {
+// 	var_dump($article);
+$res = mysqli_query($db, "SELECT messages.*, users.login FROM messages, users WHERE users.id=message.id_author");
+	require("views/messages.phtml");
+
+	
+// }
+
+
+
+
+?>
