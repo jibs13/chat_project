cf8559229e79dd26e083fa9511836cc8cd52a3da
mimimolette <maudfroehlicher@ymail.com>
2017-02-20 11:12:10 +0100

    message .php

--- apps/message.php
+++ apps/message.php
@@ -1,6 +1,8 @@
 <?php
+$res = mysqli_query($db, "SELECT messages.*, users.login FROM messages, users WHERE users.id=messages.id_author ORDER BY date DESC");
 while ($message = mysqli_fetch_assoc($res))
 {
 	require('views/message.phtml');
 }
 ?>
+
