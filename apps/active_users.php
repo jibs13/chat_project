b9f5c3e87e372c58200da25934fa75c234a09869
mimimolette <maudfroehlicher@ymail.com>
2017-02-20 15:38:24 +0100

    active_users.phtml

--- apps/active_users.php
+++ apps/active_users.php
@@ -1,6 +1,6 @@
 <?php
 
- $res = mysqli_query($db, "SELECT users.* FROM users WHERE CURRENT_TIMESTAMP = users.status ");
+ $res = mysqli_query($db, "SELECT users.* FROM users WHERE status+10>CURRENT_TIMESTAMP  ");
 while ($users = mysqli_fetch_assoc($res))
 {
 	require('views/active_users.phtml');
